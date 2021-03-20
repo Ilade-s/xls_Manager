d3507372f47e507357200b5c3b6b3a55dbe11d8c
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-03-20 22:36:23 +0100

    upgrade func DiagrammeBarres

--- xlsPlot.py
+++ xlsPlot.py
@@ -16,14 +16,13 @@ UTILISATION :
 
 FONCTIONS :
 ----------
-    - DiagrammeBarres : Utlisant une seule colonne, va créer une graphique en barres
-    - DiagrammeMultiBarres : Utlisant une colonne de clé, va créer un graphique en barres avec plusieurs colonnes de données
+    - DiagrammeBarres : Utlisant une colonne de clé, va créer un graphique en barres avec plusieurs colonnes de données
     - DiagrammeCirculaire : Utlisant une seule colonne, permet de comparer leur part dans la somme avec un camembert
 """
 
 import xlrd # Module de gestion mère xls
 import matplotlib.pyplot as plt # Création de graphiques
-import sys # Messages d'erreur
+import pandas as pd # Pour DataFrame
 
 class xlsDB:
     def __init__(self, sheet=10, fileName="pop-16ans-dipl6817"):
@@ -48,22 +47,25 @@ class xlsDB:
         self.Title = self.Data.cell_value(0,0)
         print("Test :",self.Title)
 
-    def DiagrammeBarres(self, SortedElements=(False, False), DataColumn=3, KeyColumn=2, Start=15, Stop=None, TitleOffset=2, figSize=(20.0,20.0)):
+    def DiagrammeMultiBarres(self, SortedElements=(False, False, 0), DataColumns=[3], KeyColumn=2, Start=15, Stop=None, TitleOffset=2, figSize=(20.0,20.0)):
         """
-        Permet de créer des diagrammes en barres pour comparer les éléments d'une seule colonne
+        Permet de créer des diagrammes en barres pour comparer les éléments de une ou plusieurs colonnes de données
 
         
         PARAMETRES :
         --------
         Attention, cette focntion assume que le tableau est sous forme verticale et ne supportera pas les formes horizonales
         --------
-        SortedElements : tuple(bool, bool)
+        SortedElements : tuple(bool, bool, int)
             SortedElements[0] :
                 Indique si les données doivent être triées ou non (ordre croissant)
                     default = False
             SortedElements[1] :
                 Indique si les données doivent être triées en ordre croissant (False) ou décroissant (True)
                     default = False
+            SortedElements[2] :
+                Indique l'index de la colonne de données servant à trier les éléments (index dans DataColumns)
+                    default = 0
 
         DataColumn : int
             index de la colonne contenant les valeurs à comparer
@@ -94,143 +96,47 @@ class xlsDB:
         None
         """  
         # Vérification des paramètres
-        assert DataColumn!=KeyColumn, "Erreur : Les colonnes des données et des clés/noms sont les mêmes"
+        for c in DataColumns:
+            assert c!=KeyColumn, "Erreur : Les colonnes des données et des clés/noms sont les mêmes"
+        assert SortedElements[2]<=len(DataColumns), "Erreur : l'index de la colonne choisie n'existe pas"
         assert Stop==None or Stop>Start, "Erreur, choix d'intervalle impossible (stop<=start)"
         assert SortedElements[0] or not SortedElements[0], "Le paramètre SortedElements est invalide (non boléen)"
         assert SortedElements[1] or not SortedElements[1], "Le paramètre SortedElements est invalide (non boléen)"
         
-        # Extraction données de la feuille
-        DataList = self.Data.col_values(DataColumn, Start, Stop)
+        # Extraction données et clés de la feuille
+        DataLists = [self.Data.col_values(c, Start, Stop) for c in DataColumns]
         KeyList = self.Data.col_values(KeyColumn, Start, Stop)
 
         # Arrondi des valeurs des données
-        DataList =  [round(float(i)) for i in DataList]
+        DataLists =  [[round(float(i)) for i in DataList] for DataList in DataLists]
 
         # Vidage cases vides
-        DataList = [DataList.pop(DataList.index(i)) for i in DataList if i!=""]
+        DataLists = [
+            [DataList.pop(DataList.index(i)) for i in DataList if i!=""]
+            for DataList in DataLists]
         KeyList = [KeyList.pop(KeyList.index(i)) for i in KeyList if i!=""]
 
+        # Création liste éléments
+        ElementList = [[KeyList[i]]+[DataList[i] for DataList in DataLists] for i in range(len(KeyList))]
+        
         if SortedElements[0]:
-            # Cration liste éléments
-            ElementList = [[DataList[i], KeyList[i]] for i in range(len(DataList))]
-
             # Tri des éléments par données
             def getKey(element):
-                return element[0]
+                return element[SortedElements[2]+1]
 
             ElementList.sort(key=getKey, reverse=SortedElements[1])
 
-            # Copie dans listes DataList et KeyList
-            DataList = [e[0] for e in ElementList]
-            KeyList = [e[1] for e in ElementList]
-
         # Création figure
-        plt.figure(figsize=figSize, constrained_layout=True)
+        df = pd.DataFrame(ElementList,columns=[self.Data.cell_value(Start-TitleOffset, KeyColumn)]+[self.Data.cell_value(Start-TitleOffset, DataColumn) for DataColumn in DataColumns])
 
-        width = 2
-        x = [i*4 for i in range(len(DataList))]
+        df.plot(x=self.Data.cell_value(Start-TitleOffset, KeyColumn),
+                y=[self.Data.cell_value(Start-TitleOffset, DataColumn) for DataColumn in DataColumns],
+                kind="bar", figsize=figSize)
+        
+        plt.legend(bbox_to_anchor=(0.8,1.0))
 
-        plt.bar(x, DataList, width)
-
-        # Ajout clés et titres
-        plt.xticks(x, KeyList, rotation=90)
+        # Ajout titre
         plt.title(self.Title)
-        plt.xlabel(self.Data.cell_value(Start-TitleOffset, KeyColumn)) # Titre des clés
-        plt.ylabel(self.Data.cell_value(Start-TitleOffset, DataColumn)) # Titre des données
-        
-        # Affichage diagramme
-        plt.show()
-
-    def DiagrammeMultiBarres(self, SortedElements=(False, False), DataColumns=(3), KeyColumn=2, Start=15, Stop=None, TitleOffset=2, figSize=(20.0,20.0)):
-        """
-        Permet de créer des diagrammes en barres pour comparer les éléments d'une seule colonne
-
-        
-        PARAMETRES :
-        --------
-        Attention, cette focntion assume que le tableau est sous forme verticale et ne supportera pas les formes horizonales
-        --------
-        SortedElements : tuple(bool, bool)
-            SortedElements[0] :
-                Indique si les données doivent être triées ou non (ordre croissant)
-                    default = False
-            SortedElements[1] :
-                Indique si les données doivent être triées en ordre croissant (False) ou décroissant (True)
-                    default = False
-
-        DataColumn : int
-            index de la colonne contenant les valeurs à comparer
-                default = 3
-
-        KeyColumn : int
-            index de la colonne contenant les clés (noms) liées aux données
-                default = 2
-        
-        Start : int
-            index de la ligne de départ (inclue) des éléments à étudier
-                default = 24
-        
-        Stop : int || None
-            index de la dernière ligne (exclue) des éléments à étudier ou "auto" pour exploiter toutes les données (après start)
-                default = "auto"
-
-        TitleOffset : int
-            Indique l'écart entre le Start et le titre (permet de trouver les titres d'axes)
-                default = 2
-
-        figSize : tuple(float, float)
-            Indique la taille du diagramme (x, y), cepandant, mettre des tailles en dessous de 20 n'aura aucun effet (constained_layout activé)
-                default (recommandé pour lecture) = (20.0,20.0)
-
-        SORTIE :
-        --------
-        None
-        """  
-        # Vérification des paramètres
-        assert DataColumn!=KeyColumn, "Erreur : Les colonnes des données et des clés/noms sont les mêmes"
-        assert Stop==None or Stop>Start, "Erreur, choix d'intervalle impossible (stop<=start)"
-        assert SortedElements[0] or not SortedElements[0], "Le paramètre SortedElements est invalide (non boléen)"
-        assert SortedElements[1] or not SortedElements[1], "Le paramètre SortedElements est invalide (non boléen)"
-        
-        # Extraction données de la feuille
-        DataList = self.Data.col_values(DataColumn, Start, Stop)
-        KeyList = self.Data.col_values(KeyColumn, Start, Stop)
-
-        # Arrondi des valeurs des données
-        DataList =  [round(float(i)) for i in DataList]
-
-        # Vidage cases vides
-        DataList = [DataList.pop(DataList.index(i)) for i in DataList if i!=""]
-        KeyList = [KeyList.pop(KeyList.index(i)) for i in KeyList if i!=""]
-
-        if SortedElements[0]:
-            # Cration liste éléments
-            ElementList = [[DataList[i], KeyList[i]] for i in range(len(DataList))]
-
-            # Tri des éléments par données
-            def getKey(element):
-                return element[0]
-
-            ElementList.sort(key=getKey, reverse=SortedElements[1])
-
-            # Copie dans listes DataList et KeyList
-            DataList = [e[0] for e in ElementList]
-            KeyList = [e[1] for e in ElementList]
-
-        # Création figure
-        plt.figure(figsize=figSize, constrained_layout=True)
-
-        width = 2
-        x = [i*4 for i in range(len(DataList))]
-
-        plt.bar(x, DataList, width)
-
-        # Ajout clés et titres
-        plt.xticks(x, KeyList, rotation=90)
-        plt.title(self.Title)
-        plt.xlabel(self.Data.cell_value(Start-TitleOffset, KeyColumn)) # Titre des clés
-        plt.ylabel(self.Data.cell_value(Start-TitleOffset, DataColumn)) # Titre des données
-        
         # Affichage diagramme
         plt.show()
     
@@ -288,4 +194,4 @@ if __name__=='__main__':
 
     xls = xlsDB()
 
-    xls.DiagrammeBarres((True,True))
+    xls.DiagrammeMultiBarres((True,True,0),[4,6])
